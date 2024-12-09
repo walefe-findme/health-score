5df875f93209b78a4360c1a9b84755284f8d4ae6
Walefe Pereira <walefe@findme.id>
2024-12-09 16:13:17 -0300

    fix layout color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HealthTable from "../components/HealthTable";
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
-      <div className="flex-grow">
+      <div className="flex-grow bg-gray-900">
         <HealthTable />
       </div>
     </div>
